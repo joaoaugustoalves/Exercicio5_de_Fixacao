bc9d1c93a30ace5f212f0dd8144a7eb3a9185eb7
FIC <FIC@info.local>
2020-12-14 15:52:43 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace exercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int num = 0;
+            int resultado = 0;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Tabuada do 1 ao 10: ");
+            Console.ResetColor();
+            Console.WriteLine();
+
+                
+                for (int k = 1; k <= 10; k++) {
+                    
+                        num = k;                    
+                    
+                    
+                    for (int i = 0; i <= 10; i++)
+                    {
+
+                        Console.WriteLine();
+                        
+                        resultado = i * num;
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.WriteLine($"{i} x {num} = {resultado}"); 
+
+                        Console.ResetColor();
+
+                    }
+                }
+             
+              
+        }
+    }
+}
